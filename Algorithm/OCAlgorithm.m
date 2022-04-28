d1138f5ac8bca0dee685be06f5d4d2e2787f30ae
shanpengtao <shanpengtao@58.com>
2022-04-28 16:45:23 +0800

    dev：OC第8、9题

--- Algorithm/OCAlgorithm.m
+++ Algorithm/OCAlgorithm.m
@@ -38,8 +38,16 @@
     NSLog(@"r6：%@", r6);
     
     // 第七题
-    long r7 = [self r7:-16158];
+    NSInteger r7 = [self r7:-16158];
     NSLog(@"r7：%ld", r7);
+    
+    // 第八题
+    NSInteger r8 = [self r8:@"  +232  "];
+    NSLog(@"r8：%ld", r8);
+    
+    // 第九题
+    BOOL r9 = [self r9:1223221];
+    NSLog(@"r9：%d", r9);
 }
 
 /*
@@ -278,5 +286,73 @@ static NSInteger maxLength = 0;
     
     return res;
 }
+
+/*
+ 8. 字符串转换整数 (atoi)
+ 请你来实现一个 myAtoi(string s) 函数，使其能将字符串转换成一个 32 位有符号整数（类似 C/C++ 中的 atoi 函数）。
+ 函数 myAtoi(string s) 的算法如下：
+ 读入字符串并丢弃无用的前导空格
+ 检查下一个字符（假设还未到字符末尾）为正还是负号，读取该字符（如果有）。 确定最终结果是负数还是正数。 如果两者都不存在，则假定结果为正。
+ 读入下一个字符，直到到达下一个非数字字符或到达输入的结尾。字符串的其余部分将被忽略。
+ 将前面步骤读入的这些数字转换为整数（即，"123" -> 123， "0032" -> 32）。如果没有读入数字，则整数为 0 。必要时更改符号（从步骤 2 开始）。
+ 如果整数数超过 32 位有符号整数范围 [−231,  231 − 1] ，需要截断这个整数，使其保持在这个范围内。具体来说，小于 −231 的整数应该被固定为 −231 ，大于 231 − 1 的整数应该被固定为 231 − 1 。
+ 返回整数作为最终结果。
+ 注意：
+ 本题中的空白字符只包括空格字符 ' ' 。
+ 除前导空格或数字后的其余字符串外，请勿忽略 任何其他字符。
+ */
+- (NSInteger)r8:(NSString *)str
+{
+    NSCharacterSet *set = [NSCharacterSet whitespaceCharacterSet];
+    NSString *s = [str stringByTrimmingCharactersInSet:set];
+    if (s.length == 0) return 0;
+    
+    NSInteger flag = 1;
+    NSInteger res = 0;
+    NSString *first = [s substringWithRange:NSMakeRange(0, 1)];
+    if ([first isEqualToString:@"-"]) {
+        flag = -1;
+        s = [s substringFromIndex:1];
+    }
+    else if ([first isEqualToString:@"+"]) {
+        s = [s substringFromIndex:1];
+    }
+    
+    NSArray *array = @[@"0",@"1",@"2",@"3",@"4",@"5",@"6",@"7",@"8",@"9",@"10"];
+    for (int i = 0; i < s.length; i++) {
+        NSString *c = [s substringWithRange:NSMakeRange(i, 1)];
+        if ([array containsObject:c]) {
+            res = res * 10 + c.integerValue;
+        }
+        else {
+            break;
+        }
+    }
+    res *= flag;
+    
+    
+    return MAX(MIN(res, pow(2, 31) - 1), pow(-2, 31));
+}
+
+/*
+ 9. 回文数
+ 给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。
+ 回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+ 例如，121 是回文，而 123 不是。
+ 示例 1：
+ 输入：x = 121
+ 输出：true
+ */
+- (BOOL)r9:(NSInteger)x
+{
+    NSString *s = [NSString stringWithFormat:@"%ld",x];
+    NSMutableString *str = [NSMutableString string];
+    for (NSInteger i = s.length - 1; i >= 0; i--) {
+        [str appendString:[s substringWithRange:NSMakeRange(i, 1)]];
+    }
+    
+    return [str isEqualToString:s];
+}
+
 
 @end
